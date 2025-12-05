e3235308a3b37e386c6fac27795b026177fe994c
makeentosch <artem.irk45@gmail.com>
2025-12-05 23:23:08 +0300

    NotificationSentEvent added

---
+++ Core/Core.Domain/Events/NotificationSentEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Domain.Events;
+
+/// <summary>
+/// Событие: Уведомление успешно отправлено провайдеру.
+/// </summary>
+public record NotificationSentEvent(
+    Guid NotificationId,
+    DateTime SentAt
+);

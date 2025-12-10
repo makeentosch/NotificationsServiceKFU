04f8e037e3fa9115f5329404304364b5bf3fc10d
Bulat Khafizov <hafizov.bulat2002@gmail.com>
2025-12-10 19:26:44 +0300

    Разработка Push

---
+++ PushService/PushService.Application/Interfaces/IPushNotificationService.cs
@@ -0,0 +1,6 @@
+﻿namespace PushService.Application.Interfaces;
+
+public interface IPushNotificationService
+{
+    Task SendAsync(string recipientContact, string text, CancellationToken cancellationToken);
+}

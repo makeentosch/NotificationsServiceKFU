981f58d512e1a479a8b176be8ccf283f83d1d331
Bulat Khafizov <hafizov.bulat2002@gmail.com>
2025-12-15 09:57:41 +0300

    Доработка mail

---
+++ MailService/MailService.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Core.Infrastructure.MassTransit;
+using MailService.Application.Interfaces;
+using MailService.Infrastructure.Consumers;
+using MailService.Infrastructure.Senders;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MailService.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddMailInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IMailSender, FakeMailSender>();
+        services.AddInfrastructureMassTransit(configuration, typeof(MailNotificationCreatedConsumer).Assembly);
+
+        return services;
+    }
+}
